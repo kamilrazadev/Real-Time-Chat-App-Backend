7a24c19926a75a7f84c21aba018d3e34acc3d691
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-04-11 21:41:14 +0500

    cors origin removed

--- index.js
+++ index.js
@@ -15,12 +15,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
